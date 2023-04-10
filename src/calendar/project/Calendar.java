bcbddac7f19d2f14912c0cda943db07231a02d7e
WOOSUNG <WOOSUNG@DESKTOP-1G5IH53>
2023-04-10 16:06:27 +0900

    find max days

--- src/calendar/project/Calendar.java
+++ src/calendar/project/Calendar.java
@@ -1,5 +1,7 @@
 package calendar.project;
 
+import java.util.Scanner;
+
 public class Calendar {
 
 	public static void main(String[] args) {
@@ -10,6 +12,25 @@ public class Calendar {
 		System.out.println("8  9  10 11 12 13 14");
 		System.out.println("15 16 17 18 19 20 21");
 		System.out.println("22 23 24 25 26 27 28");
+		
+		//숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
+		System.out.println("달을 입력하세요.");
+		Scanner scan = new Scanner(System.in);
+//		String number = scan.next();
+		// int num = scan.nextInt(); 하면 한번에 처리
+//		int num = Integer.parseInt(number);
+		int num = scan.nextInt();
+//		String temp = "1,2,3,4,5,6,7,8,9,10,11,12";
+//		String[] month = temp.split(",");
+		int[] month = {1,2,3,4,5,6,7,8,9,10,11,12};
+		
+		if (1<=num && num<=12) {
+			System.out.println(num + "월은 " + month[num-1] + "일까지 있습니다.");
+			
+		}else {
+			System.out.println("잘못입력했습니다.");
+		}
+		scan.close();
 	}
 
 }
