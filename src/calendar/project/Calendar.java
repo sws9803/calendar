9302fdd946112e65a7fd2e87018508ef6538bb1f
WOOSUNG <WOOSUNG@DESKTOP-1G5IH53>
2023-04-10 14:36:14 +0900

    first commit

---
+++ src/calendar/project/Calendar.java
@@ -0,0 +1,10 @@
+package calendar.project;
+
+public class Calendar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello, calendar");
+	}
+
+}
